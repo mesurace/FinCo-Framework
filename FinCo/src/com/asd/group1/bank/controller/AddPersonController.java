ded4054924c25de18a2f796259fa92ed321c6ce1
Suresh Adhikari <mesuraceer@gmail.com>
2017-02-07 18:14:28 -0600

    changes on bank

--- FinCo/src/com/asd/group1/bank/controller/AddPersonController.java
+++ FinCo/src/com/asd/group1/bank/controller/AddPersonController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.asd.group1.bank.controller;
 
 import java.awt.event.ActionEvent;
@@ -23,46 +19,46 @@ import com.asd.group1.framework.lib.factory.CustomerFactory;
 import com.asd.group1.framework.lib.factory.FincoFactory;
 import com.asd.group1.singleton.SingletonProvider;
 
-
 public class AddPersonController extends AccountController {
 
-    public AddPersonController() {
-        super();
-    }
+	public AddPersonController() {
+		super();
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        AccountFrm accountFrm = SingletonProvider.getInstanceAccountFrm();
-        PersonDialog pac = new PersonDialog(accountFrm);
-        pac.setBounds(450, 20, 300, 330);
-        pac.show();
-    }
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		AccountFrm accountFrm = SingletonProvider.getInstanceAccountFrm();
+		PersonDialog pac = new PersonDialog(accountFrm);
+		pac.setBounds(450, 20, 300, 330);
+		pac.show();
+	}
 
-    public void createAccount(MyAccountType accountType, String name, String ct, String st, String str, String zip, String acnr, String bd, String em) {
-        
-        
-        IAccount account=((AccountFactory)FincoFactory.getFactory(MyAccountType.MYAC)).getAccount(accountType);
-    	ICustomer customer=((CustomerFactory)FincoFactory.getFactory(Types.CUSTOMER)).getCustomer(PartyType.PERSONAL);
-        AAccount aAccount = (AAccount) account;
-        aAccount.setAcctNumber(acnr);
+	public void createAccount(MyAccountType accountType, String name, String ct, String st, String str, String zip,
+			String acnr, String bd, String em) {
 
-        account = aAccount;
+		IAccount account = ((AccountFactory) FincoFactory.getFactory(MyAccountType.MYAC)).getAccount(accountType);
+		ICustomer customer = ((CustomerFactory) FincoFactory.getFactory(Types.CUSTOMER))
+				.getCustomer(PartyType.PERSONAL);
+		AAccount aAccount = (AAccount) account;
+		aAccount.setAcctNumber(acnr);
 
-        Person c = (Person) customer;
-        c.setName(name);
-        c.setCity(ct);
-        c.setState(st);
-        c.setStreet(str);
-        c.setZip(zip);
-        try{
-            c.setDateOfBirth(new Date(bd));
-        }catch(IllegalArgumentException e){
-            System.err.println("IllegalArgumentException in setDateOfBirth");
-        }
-        c.setEmail(em);
-        customer = c;
-        customer.addAccount(account);
-        SingletonProvider.getInstanceAccountManager().addAccountToList(account);
+		account = aAccount;
 
-    }
+		Person c = (Person) customer;
+		c.setName(name);
+		c.setCity(ct);
+		c.setState(st);
+		c.setStreet(str);
+		c.setZip(zip);
+		try {
+			c.setDateOfBirth(new Date(bd));
+		} catch (IllegalArgumentException e) {
+			System.err.println("IllegalArgumentException in setDateOfBirth");
+		}
+		c.setEmail(em);
+		customer = c;
+		customer.addAccount(account);
+		SingletonProvider.getInstanceAccountManager().addAccountToList(account);
+
+	}
 }
--- FinCo/src/com/asd/group1/bank/controller/DepositController.java
+++ FinCo/src/com/asd/group1/bank/controller/DepositController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.asd.group1.bank.controller;
 
 import java.awt.event.ActionEvent;
@@ -12,20 +7,19 @@ import com.asd.group1.framework.gui.AccountFrm;
 import com.asd.group1.framework.gui.JDialog_Dialogview;
 import com.asd.group1.singleton.SingletonProvider;
 
-
 public class DepositController extends com.asd.group1.framework.gui.controller.DepositController {
 
-    public DepositController() {
-        super();
-    }
+	public DepositController() {
+		super();
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        AccountFrm accountFrm = SingletonProvider.getInstanceAccountFrm();
-        String accnr = accountFrm.getAccountNo();
-        JDialog_Dialogview dep = new DepositDialog(accountFrm, accnr);
-        dep.setBounds(430, 15, 275, 140);
-        dep.show();
-    }
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		AccountFrm accountFrm = SingletonProvider.getInstanceAccountFrm();
+		String accnr = accountFrm.getAccountNo();
+		JDialog_Dialogview dep = new DepositDialog(accountFrm, accnr);
+		dep.setBounds(430, 15, 275, 140);
+		dep.show();
+	}
 
 }
